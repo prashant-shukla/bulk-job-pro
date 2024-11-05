cbac395de2800cd45b89c5031c832f2fc342f3ee
Prashant Shukla <android.prashant.ps@gmail.com>
2024-11-05 12:45:52 +0530

    updated few things

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,15 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use Shopify\Clients\Rest;
+use PHPShopify\ShopifySDK;
 
 class HomeController extends Controller
 {
     public function welcome()
     {
+        $shop = Auth::user();
+        $request = $shop->api()->rest('GET', '/admin/shop.json');
+        // $request = $shop->api()->graph('{ shop { name } }');
+        echo "HERE: ";
+        echo $request['body']['shop']['name'];
+
+        $shopify = ShopifySDK::config($this->getShopifyConfig());
+        $products = $shopify->Product->get();
+        $totalProducts = count($products);    
+
         $shop = Auth::user(); // This assumes `name`, `email`, and other fields are present
         return view('welcome', [
             'shopDomain' => $shop->name,
             'shopEmail' => $shop->email,
+            'totalProducts' => $totalProducts
         ]);
     }
 
