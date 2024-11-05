a17b980ac832a5c89d559428085eb24d60eef135
Prashant Shukla <android.prashant.ps@gmail.com>
2024-11-05 17:28:21 +0530

    testing graph#3

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,7 +12,20 @@ class HomeController extends Controller
         // $response = $shop->api()->rest('GET', '/admin/shop.json');
         $response = $shop->api()->graph('{ shop { name } }');
 
-        $response_products = $shop->api()->graph('{ products(first: 10) }');
+        $response_products = $shop->api()->graph('
+                { products(first: 10) {
+                    edges {
+                        node {
+                        id
+                        title
+                        handle
+                        }
+                        cursor
+                    }
+                    pageInfo {
+                        hasNextPage
+                    } 
+                } } ');
 
         echo '<pre> HERE : <hr />';
         print_r(json_encode($response));
