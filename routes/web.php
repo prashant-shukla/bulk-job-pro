9998dea3998eae957505906651858eadc5d2f48e
Prashant Shukla <android.prashant.ps@gmail.com>
2024-11-05 18:33:39 +0530

    updated products#9

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 
 Route::middleware(['verify.shopify'])->group(function () {
-    // Route::get('/', [HomeController::class, 'welcome'])->name('home');
+    Route::get('/home', [HomeController::class, 'welcome'])->name('home');
     // Route::get('/admin/select-products', [ProductController::class, 'index'])->name('admin.select-products');
-    Route::get('/', [ProductController::class, 'index'])->name('home');
+    Route::get('/', [ProductController::class, 'index'])->name('admin.select-products');
     Route::post('/admin/save-products', [ProductController::class, 'saveSelection'])->name('admin.save-products');
 });
 
